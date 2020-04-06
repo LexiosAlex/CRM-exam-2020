fb1b0d1cd93fc8907029d583b73e1b959ef02586
LexiosAlex <lexiosma@gmail.com>
2020-04-06 19:23:35 +0300

    added firebase with env to project

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,4 +1,19 @@
 import express from 'express';
+import './env/env';
+
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: `${process.env.FIREBASE_SENDER_ID}`,
+  appId: `${process.env.FIREBASE_APP_ID}`,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const app: express.Application = express();
 
