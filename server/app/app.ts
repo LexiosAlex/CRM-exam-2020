0053aea09fb303973cfc088579971eb9defc16b2
LexiosAlex <lexiosma@gmail.com>
2020-04-06 22:55:27 +0300

    code review changes

--- server/app/app.ts
+++ server/app/app.ts
@@ -3,28 +3,10 @@ import path from 'path';
 
 import './env/env';
 
-import * as firebase from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: `${process.env.FIREBASE_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`,
-};
-
-firebase.initializeApp(firebaseConfig);
-
 const app: express.Application = express();
 
-app.use('/static', express.static(path.join(__dirname, '../../client/build')));
+app.use('/', express.static(path.join(__dirname, `${process.env.CLIENT_BUILD_PATH}`)));
 
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
-
-app.listen(3050, () => {
-  console.log('Example app listening on port 3050!');
+app.listen(`${process.env.PORT}`, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
 });
