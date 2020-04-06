01a4b0b1627f98771b3a564f7263f0f7b110d1bd
dhilt <d.alexanov@gmail.com>
2020-04-06 04:38:48 +0300

    prettier generalization

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,11 +1,11 @@
-import express = require("express");
+import express from 'express';
 
 const app: express.Application = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(3050, function () {
-  console.log("Example app listening on port 3050!");
+app.listen(3050, () => {
+  console.log('Example app listening on port 3050!');
 });
