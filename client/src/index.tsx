e0990bddc54e0675e96ff4f8d8def907342259e0
dhilt <d.alexanov@gmail.com>
2020-04-11 16:14:37 +0300

    .env on client, firebase config single source

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { firebaseConfig } from 'common/firebase.config';
+import Firebase from './firebase';
+
 import App from './components/App';
+
+const FB = new Firebase(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,18 @@
 const path = require('path');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const _env = dotenv.config({ path: path.resolve(__dirname, '../.env') });
+const env = _env.parsed;
+if (_env.error instanceof Error) {
+  throw _env.error;
+}
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   resolve: {
@@ -79,5 +91,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new webpack.DefinePlugin(envKeys),
   ],
 };
