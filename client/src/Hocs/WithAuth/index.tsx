e603ac4fc011ee07a8b3aa3e3866962f0879c28f
dhilt <d.alexanov@gmail.com>
2020-04-23 15:45:38 +0300

    receive activity list by SET_PROFILE

--- client/src/Hocs/WithAuth/index.tsx
+++ client/src/Hocs/WithAuth/index.tsx
@@ -3,7 +3,7 @@ import { AppState } from '../../reducers/rootReducer';
 
 export default connect((state: AppState) => {
   const { firebase } = state;
-  const {auth, profile, authError} = firebase;
+  const { auth, profile, authError } = firebase;
   return {
     auth,
     profile,
--- client/src/epics/root.ts
+++ client/src/epics/root.ts
@@ -1,40 +1,42 @@
 import { combineEpics, Epic, ofType, StateObservable } from 'redux-observable';
+import firebase from 'firebase/app';
 import { mergeMap, map } from 'rxjs/operators';
-import { ajax } from 'rxjs/ajax';
-import { Observable } from 'rxjs';
-import { AppState } from '../reducers/rootReducer';
+import { Observable, from } from 'rxjs';
+
 import { Action } from 'typesafe-actions';
+import { AppState } from '../reducers/rootReducer';
 import { REFS } from '../utils/refs';
-import { useFirebase } from 'react-redux-firebase';
-import { from } from 'rxjs';
 
 const firebasePrefix: string = '@@reactReduxFirebase';
 
-const getQuery = (action, state) => {
-  const { auth } = state;
-  const { type } = action.profile;
-  const firebase = useFirebase();
-  const ActivitiesRef = firebase.ref(REFS.ACTIVITIES);
+const getQuery = ({
+  profile: { type },
+  auth: { uid },
+}): /*firebase.database.Reference | firebase.database.Query*/ any => {
+  const ActivitiesRef = firebase.database().ref(REFS.ACTIVITIES);
   switch (type) {
     case 0:
       return ActivitiesRef;
     case 1:
-      return ActivitiesRef.orderByChild('operator').equalTo(auth.uid);
+      return ActivitiesRef.orderByChild('operator').equalTo(uid);
     case 2:
-      return ActivitiesRef.orderByChild('assignee').equalTo(auth.uid);
+      return ActivitiesRef.orderByChild('assignee').equalTo(uid);
     default:
       return ActivitiesRef;
   }
 };
 
-const fetchDataFulfilled = activities => ({ type: 'FETCH_ACTIVITIES', payload: activities });
+const fetchDataFulfilled = (activities) => ({ type: 'FETCH_ACTIVITIES', payload: activities });
 
-const fetchActivities: Epic<Action, Action<any>, AppState> = (action$, state$) =>
+const fetchActivities: Epic<Action<string>, Action<any>, AppState> = (action$, state$) =>
   action$.pipe(
     ofType(`${firebasePrefix}/SET_PROFILE`),
-    mergeMap((action: any) =>
-      getQuery(action, state$.value.firebase).on('value', snap => {
-        return from(snap.val()).pipe(map(activities => fetchDataFulfilled(activities)));
+    map(() =>
+      getQuery(state$.value.firebase).on('value', (snap) => {
+        const data = snap.val();
+        console.log('received: ', data);
+        return fetchDataFulfilled(data);
+        // return from(snap.val()).pipe(map((activities) => fetchDataFulfilled(activities)));
       })
     )
   );
