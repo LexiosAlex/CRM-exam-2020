7a9128ac946e9fc37e6964d270b92dabef704465
LexiosAlex <lexiosma@gmail.com>
2020-04-27 20:14:29 +0300

    epics

--- client/src/epics/activities.ts
+++ client/src/epics/activities.ts
@@ -1,10 +1,16 @@
 import { Epic, ofType } from 'redux-observable';
 import firebase from 'firebase/app';
-import { switchMap } from 'rxjs/operators';
+import { mergeMap, concat, switchMap, map } from 'rxjs/operators';
 import { Action } from 'typesafe-actions';
+import { Observable, of } from 'rxjs';
 
 import { AppState } from '../reducers/rootReducer';
 import { REFS } from '../utils/refs';
+import {
+  GET_ACTIVITIES_DONE,
+  GET_ACTIVITIES_FAIL,
+  GET_ACTIVITIES_PENDING,
+} from '../interfaces/actions/activities';
 
 const firebasePrefix: string = '@@reactReduxFirebase';
 
@@ -17,25 +23,36 @@ const getQuery = ({
     case 0:
       return ActivitiesRef.once('value');
     case 1:
-      return ActivitiesRef.orderByChild('operator').equalTo(uid).once('value');
+      return ActivitiesRef.orderByChild('operator')
+        .equalTo(uid)
+        .once('value');
     case 2:
-      return ActivitiesRef.orderByChild('assignee').equalTo(uid).once('value');
+      return ActivitiesRef.orderByChild('assignee')
+        .equalTo(uid)
+        .once('value');
     default:
       return Promise.reject('Bad Employee type');
   }
 };
 
-const fetchDataFulfilled = (payload) => ({ type: 'GET_ACTIVITIES_DONE', payload });
-const fetchDataFailed = (payload) => ({ type: 'GET_ACTIVITIES_FAIL', payload });
+const fetchDataRequested = () => ({ type: GET_ACTIVITIES_PENDING });
+const fetchDataFulfilled = payload => ({ type: GET_ACTIVITIES_DONE, payload });
+const fetchDataFailed = payload => ({ type: GET_ACTIVITIES_FAIL, payload });
+
+const startFetchActivities: Epic<Action<string>, Action<any>, AppState> = (action$, state$) =>
+  action$.pipe(
+    ofType(`${firebasePrefix}/SET_PROFILE`),
+    map(() => fetchDataRequested())
+  );
 
 const fetchActivities: Epic<Action<string>, Action<any>, AppState> = (action$, state$) =>
   action$.pipe(
     ofType(`${firebasePrefix}/SET_PROFILE`),
     switchMap(() =>
       getQuery(state$.value.firebase)
-        .then((data) => fetchDataFulfilled({ activities: data.val() }))
-        .catch((error) => fetchDataFailed({ error }))
+        .then(data => fetchDataFulfilled(data.val()))
+        .catch(error => fetchDataFailed({ error }))
     )
   );
 
-export default [fetchActivities];
+export default [startFetchActivities, fetchActivities];
