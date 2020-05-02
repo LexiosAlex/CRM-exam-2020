afd3c86b848a118e4e7f5689c83f30a39836b27c
dhilt <d.alexanov@gmail.com>
2020-05-02 22:02:20 +0300

    checkStatus

--- client/src/selectors/activities.ts
+++ client/src/selectors/activities.ts
@@ -3,23 +3,22 @@ import { createSelector } from 'reselect';
 import userSelectors from './user';
 import { ActivityStatus, EmployeeType, IActivity } from 'common/index';
 import { IAppState, IActivitiesState } from 'src/reducers/rootReducer';
-import {
-  isActivityVisible,
-  TITLE_STATUS_MAP,
-  VOLUNTEER_ACTIVITY_STATUSES,
-} from 'src/utils/activities';
+import { checkStatus, TITLE_STATUS_MAP, VOLUNTEER_ACTIVITY_STATUSES } from 'src/utils/activities';
 
 const getActivities = (state: IAppState): IActivitiesState => state.activities;
 
-const getHeap = createSelector([getActivities], activities => activities.heap);
+const getHeap = createSelector([getActivities], (activities) => activities.heap);
 
-const isEmpty = createSelector([getActivities], activities => !Object.keys(activities.heap).length);
+const isEmpty = createSelector(
+  [getActivities],
+  (activities) => !Object.keys(activities.heap).length
+);
 
 const getFilteredHeap = createSelector([userSelectors.getEmployeeType, getHeap], (type, heap) =>
   Object.entries(heap).reduce(
     (acc: { [key: string]: IActivity }, [key, activity]) => ({
       ...acc,
-      ...(isActivityVisible(type, activity) ? { [key]: activity } : {}),
+      ...(checkStatus(type, activity.status) ? { [key]: activity } : {}),
     }),
     {} as any
   )
@@ -27,22 +26,14 @@ const getFilteredHeap = createSelector([userSelectors.getEmployeeType, getHeap],
 
 const getLists = createSelector([userSelectors.getEmployeeType, getFilteredHeap], (type, heap) =>
   Object.entries(heap).reduce(
-    (
-      acc: {
-        [key in ActivityStatus]: IActivity[];
-      },
-      [id, activity]
-    ) => ({
+    (acc: { [key in ActivityStatus]: IActivity[] }, [id, activity]) => ({
       ...acc,
-      [activity.status]: [...(acc[activity.status] || []), { ...activity, id }],
+      [activity.status]: [...acc[activity.status], { ...activity, id }],
     }),
     Object.values(ActivityStatus).reduce(
-      (acc, value) => ({
+      (acc, status) => ({
         ...acc,
-        ...(typeof value === 'number' &&
-        isActivityVisible(type, { status: value as any } as IActivity)
-          ? { [value]: [] }
-          : {}),
+        ...(typeof status === 'number' && checkStatus(type, status) ? { [status]: [] } : {}),
       }),
       {} as any
     )
