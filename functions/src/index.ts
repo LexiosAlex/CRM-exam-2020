07160646a55e142cfd99fa532dc3dbb8411dac8e
LexiosAlex <lexiosma@gmail.com>
2020-05-14 04:54:28 +0300

    Revert "change Activity status via firebase functions"
    This reverts commit 2e992d491216545b636fecbb10d31082b3d06755.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-import { EmployeeType, ActivityStatus, getAllowedStatuses } from '../../common/index';
+import { EmployeeType } from '../../common/index';
 
 admin.initializeApp();
 
@@ -18,57 +18,9 @@ export const processSignUp = functions.auth.user().onCreate(async ({ uid }) => {
     console.error(e);
     return;
   }
+  // // Update real-time database to notify client to force refresh.
+  // const metadataRef = admin.database().ref('metadata/' + uid);
+  // // Set the refresh time to the current UTC timestamp.
+  // // This will be captured on the client to force a token refresh.
+  // return metadataRef.set({ refreshTime: new Date().getTime() });
 });
-
-const getEmployeeType = (context: functions.https.CallableContext): Promise<EmployeeType> =>
-  new Promise(async (resolve, reject) => {
-    if (!context.auth) {
-      return reject('Not signed in');
-    }
-    try {
-      const type = await admin
-        .auth()
-        .getUser((context.auth as any).uid)
-        .then(({ customClaims }) => resolve((customClaims as any).type));
-      if (type !== void 0) {
-        resolve(type as any);
-      } else {
-        reject('Employee type error (1)');
-      }
-    } catch (e) {
-      console.error(e);
-      reject('Employee type error (2)');
-    }
-  });
-
-const setActivityStatus = async (
-  employeeType: EmployeeType,
-  activityId: string,
-  status: ActivityStatus
-) => {
-  const activityRef = admin.database().ref(`/activities/${activityId}`);
-  const activity = await activityRef.once('value').then((data) => data.val());
-  const allowed = getAllowedStatuses(employeeType, activity.status);
-  const isAllowed = allowed.some((s) => s === status);
-  if (!isAllowed) {
-    throw 'Transition not permitted';
-  }
-  await activityRef.update({ status });
-};
-
-export const changeActivityStatus = functions.https.onCall(
-  ({ id, status }, context) =>
-    new Promise(async (resolve, reject) => {
-      try {
-        if (!id || !status) {
-          throw 'Invalid params';
-        }
-        const type = await getEmployeeType(context);
-        await setActivityStatus(type, id, status);
-        resolve({ error: null });
-      } catch (error) {
-        console.error(error);
-        resolve({ error });
-      }
-    })
-);
